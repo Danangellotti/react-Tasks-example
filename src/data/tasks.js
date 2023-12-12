f6c5ad5213dadf44b5a00a76823d79186e9f2e36
Dana Angellotti <94261151+Danangellotti@users.noreply.github.com>
2023-12-12 10:30:19 -0300

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,22 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "mi primer tarea",
+    description: "mi primer tarea",
+  },
+  {
+    id: 1,
+    title: "mi segunda tarea",
+    description: "mi primer tarea",
+  },
+  {
+    id: 2,
+    title: "mi tercera tarea",
+    description: "mi primer tarea",
+  },
+  {
+    id: 3,
+    title: "mi cuarta tarea",
+    description: "mi primer tarea",
+  },
+];
